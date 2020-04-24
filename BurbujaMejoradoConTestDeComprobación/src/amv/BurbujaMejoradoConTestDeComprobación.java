42b722ca0f781c789fe40901cceb33a915714ffc
alemarviz <alemarviz.cma@gmail.com>
2020-04-24 11:35:00 +0200

    Comentarios

--- BurbujaMejoradoConTestDeComprobación/src/amv/BurbujaMejoradoConTestDeComprobación.java
+++ BurbujaMejoradoConTestDeComprobación/src/amv/BurbujaMejoradoConTestDeComprobación.java
@@ -15,13 +15,15 @@ public class BurbujaMejoradoConTestDeComprobación {
 		}
 		
 		// Algoritmo de ordenación
+		// tope controla el número de pasadas y el límite de la misma
 		for(int tope = datos.length-1; tope >= 1; tope--) 
 		{
 			// test de comprobación
 			boolean sw = true; 
 			for(int i = 1; i <= tope ; i++) 
 			{
-				if (datos[i-1]>datos[i])
+				// Ordenación creciente
+				if (datos[i-1] > datos[i])
 				{
 					int temp = datos[i-1];
 					datos[i-1] = datos[i];
@@ -29,9 +31,12 @@ public class BurbujaMejoradoConTestDeComprobación {
 					sw = false;
 				}
 			} // fin del for interno
+			
+			// Si durante la pasada no se ha realizado intercambios, podemos detener el proceso
 			if (sw) break;
 		}
 		// Fin del proceso. 
+		
 		// Mostramos el vector resultado ordenado
 		System.out.print("Vector: ");
 		for(int valor: datos)
